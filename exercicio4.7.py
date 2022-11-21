53d1b4b39ac65632a7747e6c85ab438d3ec0cdd6
caiorev <caio.rennan@estudante.ifmt.edu.br>
2022-11-21 15:08:33 +0000

    arquivos

--- exercicio4.7.py
+++ exercicio4.7.py
@@ -2,6 +2,9 @@ compra = []
 venda = []
 mercadoria = []
 mcdrs = 100
+lucro = 0 
 for mercadorias in range(mcdrs)
     compra.append = float(input('Valor de compra: '))
     venda.append = float(input('Valor de venda: '))
+    
+for marcadorias in range(mcdrs)
